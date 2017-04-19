f435783e2aa81e2fb140452b340b697669b4a4ca
Gabriel Salas <gabesal96@gmail.com>
2017-04-18 18:42:39 -0700

    states for game object

---
+++ app/js/gameover.js
@@ -0,0 +1,39 @@
+Battleship.GameOverState = Battleship.GameOverState || {};
+//  properties
+Battleship.GameOverState.intro_text;
+
+Battleship.GameOverState.init = function () {
+
+};
+
+Battleship.GameOverState.preload = function () {
+  
+};
+
+Battleship.GameOverState.create = function () {
+  this.game.stage.backgroundColor = '#222';
+
+  var bar = this.game.add.graphics();
+  bar.beginFill(0x000000, 0.2);
+  bar.drawRect(0, 300, 800, 100);
+
+  var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+
+  // Text is positioned at 0, 100
+  this.intro_text = this.game.add.text(0, 0, "Tap Screen to Restart", style);
+  this.intro_text.setShadow(3, 3, 'rgba(0, 0, 0, 0.5)', 2);
+
+  this.intro_text.setTextBounds(-75, 300, 800, 100);
+
+  bar.inputEnabled = true;
+  bar.events.onInputDown.add(this.gameStart, this);
+
+};
+
+Battleship.GameOverState.gameStart = function(state) {
+  this.game.state.start("GameState");
+};
+
+Battleship.GameOverState.shutdown = function() {
+  console.log("shutdown game over");
+};
